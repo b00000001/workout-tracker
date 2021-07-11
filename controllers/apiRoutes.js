30c37ed3fb1ec6c760e4ea27968e7ee12c2f5e6d
Aaron Deas <deas.aaron@gmail.com>
2021-07-10 23:05:17 -0400

    Added routes for api

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,14 +1,54 @@
 const router = require("express").Router();
 const Workout = require("../models/workout");
-router.get("/workouts", async (req, res) => {
-  try{
+router.get("/api/workouts", async (req, res) => {
+  try {
     const dbWorkoutData = await Workout.find();
     return res.json(dbWorkoutData);
-  }
-  catch(err){
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
+});
+
+router.post("/api/workouts", async (req, res) => {
+  try {
+    const dbWorkoutData = await Workout.create({});
+    return res.json(dbWorkoutData);
+  } catch (err) {}
+  console.log(err);
+  res.status(500).json(err);
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+  try {
+    const dbWorkoutData = await Workout.find();
+    return res.json(dbWorkoutData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/api/workouts/range", async (req, res) => {
+  try {
+    const dbWorkoutData = await Workout.create({});
+    return res.json(dbWorkoutData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/api/workouts/:id", async (req, res) => {
+  try {
+    const DBWorkoutData = await Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      { new: true, runValidators: true }
+    );
+  } catch (err) {}
+  const dbWorkoutData = await Workout.create({});
+  return res.json(dbWorkoutData);
 });
 
 module.exports = router;
